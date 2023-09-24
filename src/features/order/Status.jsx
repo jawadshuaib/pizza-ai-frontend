e899cfd98224c4e40ecbfb4dabfdfa51495fe233
Jawad <jawad.php@gmail.com>
2023-09-24 17:59:43 -0400

    Status changed to await/async

--- src/features/order/Status.jsx
+++ src/features/order/Status.jsx
@@ -24,50 +24,44 @@ export default function Status() {
   useEffect(() => {
     if (orderId === '') return;
 
-    getOrderDetails({ orderId }).then((order) => {
-      if (order === null) return;
+    const fetchOrder = async () => {
+      try {
+        // ## Fetch order details ##
+        const order = await getOrderDetails({ orderId });
+        if (order === null) return;
 
-      // Reset all redux stores
-      dispatch(resetOrder());
-      dispatch(resetPizza());
-      dispatch(resetToppings());
+        // Reset all redux stores
+        dispatch(resetOrder());
+        dispatch(resetPizza());
+        dispatch(resetToppings());
 
-      // Save order details
-      setOrderDetails(order[0]);
-      // Get customer information
-      getCustomerDetails({ customerId: order[0]['customer_id'] }).then(
-        (customer) => {
-          // Save customer email
-          setOrderDetails((prevState) => ({
-            ...prevState,
-            email: customer[0]['email'],
-          }));
-        },
-      );
+        // Save order details
+        setOrderDetails(order[0]);
 
-      // Download image from supabase storage
-      getImageFromSupabase(order[0]['image_url']).then((image) => {
-        // Show image as UI header
+        // ## Fetch customer details ##
+        const customer = await getCustomerDetails({
+          customerId: order[0]['customer_id'],
+        });
+        // Save customer email
+        setOrderDetails((prevState) => ({
+          ...prevState,
+          email: customer[0]['email'],
+        }));
+
+        // ## Download image from supabase storage
+        const image = await getImageFromSupabase(order[0]['image_url']);
         dispatch(setHeaderImage(image));
-      });
 
-      // getAvailableToppings
-      // Get toppings selected
-      getToppingsOrdered({ orderId }).then((toppings) => {
-        // Save topping ids
+        // ## Fetch toppings selected ##
+        const toppings = await getToppingsOrdered({ orderId });
         setToppingIds(toppings.map((row) => row.topping_id));
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-        // setToppings(
-        //   availableToppings
-        //     .filter((row) => {
-        //       return toppings.includes(row.topping);
-        //     })
-        //     .map((row) => row.topping),
-        // );
-      });
-
-      setLoading(false);
-    });
+    fetchOrder();
+    setLoading(false);
   }, []);
 
   if (loading) return <Loader reason="Fetching your order details...📦" />;
