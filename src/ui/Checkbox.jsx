ea70c2e12a2d123f56bbcabba1991092e0ba6625
Jawad <jawad.php@gmail.com>
2023-09-28 02:23:46 -0400

    Added highlight to topping list:

--- src/ui/Checkbox.jsx
+++ src/ui/Checkbox.jsx
@@ -18,14 +18,18 @@ export default function Checkbox({
   }
   return (
     <>
-      <div className="flex items-center pl-3">
+      <div
+        className={`${
+          checked ? 'bg-green-100 dark:bg-gray-500' : ''
+        } flex items-center pl-3`}
+      >
         <input
           id={key}
           type="checkbox"
           value={option}
           checked={checked}
           onChange={handleChecked}
-          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+          className="w-4 h-4 text-blue-600  border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
         />
         <label
           htmlFor={key}
