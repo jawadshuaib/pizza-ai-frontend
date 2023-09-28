be7e70ad67908b3f946a1d7f3ec9701ee9a189f5
Jawad <jawad.php@gmail.com>
2023-09-28 16:58:59 -0400

    Redirect all traffic back to index.html

--- netlify/functions/cors-proxy.js
+++ netlify/functions/cors-proxy.js
@@ -32,39 +32,3 @@ export async function handler(event) {
     };
   }
 }
-
-// /* eslint-env node */
-// import fetch from 'node-fetch';
-
-// export async function handler(event) {
-//   const url = event.queryStringParameters.url;
-//   const contentType = event.queryStringParameters.contentType;
-
-//   if (!url) {
-//     return {
-//       statusCode: 400,
-//       body: 'url parameter is required',
-//     };
-//   }
-//   // console.log('URL IS:' + url);
-//   try {
-//     const response = await fetch(url);
-//     const arrayBuffer = await response.arrayBuffer();
-//     const buffer = Buffer.from(arrayBuffer);
-
-//     return {
-//       statusCode: 200,
-//       headers: {
-//         'Content-Type': contentType === undefined ? 'image/png' : contentType,
-//         'Access-Control-Allow-Origin': '*',
-//       },
-//       body: buffer.toString('base64'),
-//       isBase64Encoded: true,
-//     };
-//   } catch (error) {
-//     return {
-//       statusCode: 500,
-//       body: 'Error fetching content.',
-//     };
-//   }
-// }
