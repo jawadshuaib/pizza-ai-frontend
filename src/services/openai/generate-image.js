9787733ead93af055bcc6aab32c4d7f7bf824139
Jawad <jawad.php@gmail.com>
2023-10-01 17:39:15 -0400

    img test

--- src/services/openai/generate-image.js
+++ src/services/openai/generate-image.js
@@ -17,9 +17,9 @@ export default async function generateImage(prompt) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
-    // const data = await response.json();
-    // return data.url;
-    return 'https://oaidalleapiprodscus.blob.core.windows.net/private/org-fjrJOPSACALV6hQYyDaufQ2g/user-LZMlgSZCBAhrIkOmf8ooTgu9/img-IXtlgnEX8mTM3mblWdbu5TFt.png?st=2023-10-01T19%3A13%3A14Z&se=2023-10-01T21%3A13%3A14Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-10-01T17%3A05%3A22Z&ske=2023-10-02T17%3A05%3A22Z&sks=b&skv=2021-08-06&sig=4jNVAww1ZsxHrtH7O2U9z/RsOnLbEZ5NsvkXYl1AnNU%3D';
+    const data = await response.json();
+    return data.url;
+    // return 'https://oaidalleapiprodscus.blob.core.windows.net/private/org-fjrJOPSACALV6hQYyDaufQ2g/user-LZMlgSZCBAhrIkOmf8ooTgu9/img-IXtlgnEX8mTM3mblWdbu5TFt.png?st=2023-10-01T19%3A13%3A14Z&se=2023-10-01T21%3A13%3A14Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-10-01T17%3A05%3A22Z&ske=2023-10-02T17%3A05%3A22Z&sks=b&skv=2021-08-06&sig=4jNVAww1ZsxHrtH7O2U9z/RsOnLbEZ5NsvkXYl1AnNU%3D';
   } catch (error) {
     console.error('Fetch error:', error);
     return { error: error.message };
