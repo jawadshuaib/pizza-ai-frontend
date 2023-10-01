80b032b6d96195546ea8265184a8f82ea8328489
Jawad <jawad.php@gmail.com>
2023-10-01 05:31:24 -0400

    Supabase download image blob as buffer working:

--- netlify/functions/supabase-upload.js
+++ netlify/functions/supabase-upload.js
@@ -17,22 +17,13 @@ const blobToFile = (blob, fileName = 'temp-name.png') => {
   return new File([blob], fileName, { type: 'image/png' });
 };
 
-// const fetchImageBlob = async (imageUrl) => {
-//   const response = await fetch(imageUrl);
-//   if (!response.ok)
-//     throw new Error(`Failed to fetch the image: ${response.status}`);
-//   return await response.blob();
-// };
-
 export async function handler(event) {
   try {
     const pathArray = event.path.split('/');
     const action = pathArray[pathArray.length - 1];
-    let result;
 
     const body = JSON.parse(event.body || '{}');
-    let base64String, base64Data;
-    let base64, buffer, resp;
+    let base64String, base64Data, buffer, result;
     switch (action) {
       case 'fetchAndUploadImage':
         base64String = body.file;
@@ -45,18 +36,8 @@ export async function handler(event) {
         result = await fetchAndUploadImage(buffer);
         break;
       case 'getImageFromSupabase':
-        buffer = await getImageFromSupabase(body.path); // assuming this returns a Blob
-
-        // Convert Blob to Base64
-        base64 = buffer.toString('base64');
-        resp = {
-          statusCode: 200,
-          isBase64Encoded: true,
-          headers: { 'Content-Type': 'image/png' },
-          body: base64,
-        };
-        console.log('RESPONSE-getImageFromSupabase ', resp);
-        return resp;
+        result = await getImageFromSupabase(body.path);
+        break;
       default:
         return {
           statusCode: 404,
@@ -78,93 +59,30 @@ async function fetchAndUploadImage(blob) {
   return await uploadImageToSupabase(file);
 }
 
-// async function fetchAndUploadImage(imageUrl) {
-//   try {
-//     // Encode image URL
-//     const encodedUrl = encodeURIComponent(imageUrl);
+// Get image from Supabase Storage
+async function getImageFromSupabase(path) {
+  const { data, error } = await supabase.storage
+    .from('pizza-images')
+    .download(path);
 
-//     // Call corsProxy
-//     const response = await corsProxy({
-//       queryStringParameters: { url: encodedUrl },
-//     });
+  if (error) {
+    throw error;
+  }
+  // We uploaded a blob to Supabase, so that's what is returned to us
+  // but in a non-browser environment, we can't use a blob
+  // so we must first convert it to a Buffer
+  if (data instanceof Blob) {
+    const buffer = Buffer.from(await data.arrayBuffer());
+    const base64 = buffer.toString('base64');
+    const dataURL = `data:image/png;base64,${base64}`;
+    return dataURL;
+  } else {
+    throw new Error('Downloaded data is not a Blob');
+  }
 
-//     if (response.statusCode !== 200) {
-//       throw new Error(`Error from corsProxy: ${response.body}`);
-//     }
-
-//     // Decode Base64 to Blob
-//     const base64Content = response.body;
-//     // const blob = base64ToBlob(base64Content);
-
-//     // If you have a separate fetchImageBlob function, you might want to use it here
-//     const blob = await fetchImageBlob(base64Content);
-
-//     // Convert blob to file and upload to Supabase
-//     const file = blobToFile(blob);
-//     return await uploadImageToSupabase(file);
-//   } catch (error) {
-//     console.error('Error in fetchAndUploadImage:', error.message);
-//     return { error: error.message };
-//   }
-// }
-
-// function base64ToBlob(base64) {
-//   const byteCharacters = atob(base64);
-//   const byteArrays = [];
-
-//   for (let offset = 0; offset < byteCharacters.length; offset += 512) {
-//     const slice = byteCharacters.slice(offset, offset + 512);
-//     const byteNumbers = new Array(slice.length);
-//     for (let i = 0; i < slice.length; i++) {
-//       byteNumbers[i] = slice.charCodeAt(i);
-//     }
-//     const byteArray = new Uint8Array(byteNumbers);
-//     byteArrays.push(byteArray);
-//   }
-
-//   const blob = new Blob(byteArrays, { type: 'image/png' }); // Adjust the MIME type if needed
-//   return blob;
-// }
-
-// async function fetchAndUploadImage(imageUrl) {
-//   try {
-//     // Add proxy to image url
-//     imageUrl = encodeURIComponent(imageUrl);
-//     const response = await corsProxy({
-//       queryStringParameters: { url: imageUrl },
-//     });
-
-//     console.log('CORS PROXY RESPONSE: ', response);
-
-//     if (response.statusCode !== 200) {
-//       // Handle error, maybe throw an error or return a response with error details.
-//       throw new Error(`Error from corsProxy: ${response.body}`);
-//     }
-
-//     imageUrl = response.body;
-//     // // fetch image blob
-//     const blob = await fetchImageBlob(imageUrl);
-//     // convert blob to file
-//     const file = blobToFile(blob);
-//     // upload image to Supabase Storage
-//     return await uploadImageToSupabase(file);
-//   } catch (error) {
-//     console.error('Error in fetchAndUploadImage:', error.message);
-//     return { error: error.message };
-//   }
-// }
-
-// async function fetchAndUploadImage(blob) {
-//   // Add proxy to image url
-//   // imageUrl = encodeURIComponent(imageUrl);
-//   // imageUrl = `${corsProxy}${imageUrl}`;
-//   // // fetch image blob
-//   // const blob = await fetchImageBlob(imageUrl);
-//   // convert blob to file
-//   const file = blobToFile(blob);
-//   // upload image to Supabase Storage
-//   return await uploadImageToSupabase(file);
-// }
+  // const url = URL.createObjectURL(data);
+  // return url;
+}
 
 // Upload image to Supabase Storage
 async function uploadImageToSupabase(file) {
@@ -180,18 +98,4 @@ async function uploadImageToSupabase(file) {
   }
 
   return data;
-}
-
-// Get image from Supabase Storage
-async function getImageFromSupabase(path) {
-  const { data, error } = await supabase.storage
-    .from('pizza-images')
-    .download(path);
-
-  if (error) {
-    throw error;
-  }
-
-  const url = URL.createObjectURL(data);
-  return url;
 }
--- src/services/supabase/upload.js
+++ src/services/supabase/upload.js
@@ -1,5 +1,3 @@
-import supabase from './supabase';
-import { v4 as uuidv4 } from 'uuid';
 import { fetchImageBlob, blobToBase64 } from '../../utils/common';
 import { corsProxy } from '../../utils/settings';
 
@@ -17,8 +15,7 @@ async function callNetlifyUploadFunction(action, body) {
     if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
     return await response.json();
   } catch (error) {
-    console.error('Error calling Netlify function:', error);
-    throw error;
+    return { error: error.message };
   }
 }
 
@@ -35,62 +32,10 @@ export async function fetchAndUploadImage(imageUrl) {
   });
 }
 
-// Upload image to Supabase Storage
-export async function uploadImageToSupabase(file) {
-  const path = `public/${uuidv4()}.png`;
-  const { data, error } = await supabase.storage
-    .from('pizza-images')
-    .upload(path, file, {
-      contentType: 'image/png',
-    });
-
-  if (error) {
-    throw error;
-  }
-
-  return data;
-}
-
 // Get image from Supabase Storage
 export async function getImageFromSupabase(path) {
-  const { data, error } = await supabase.storage
-    .from('pizza-images')
-    .download(path);
-
-  if (error) {
-    throw error;
-  }
-
-  const url = URL.createObjectURL(data);
-  return url;
+  const data = await callNetlifyUploadFunction('getImageFromSupabase', {
+    path,
+  });
+  return data;
 }
-
-// export async function fetchAndUploadImage(imageUrl) {
-//   return await callNetlifyUploadFunction('fetchAndUploadImage', {
-//     imageUrl,
-//   });
-// }
-
-// export async function fetchAndUploadImage(imageUrl) {
-//   // Get development or production proxy
-//   // Add proxy to image url
-//   const baseUrl = mode.isDevelopment ? corsProxy.local : corsProxy.remote;
-//   imageUrl = encodeURIComponent(imageUrl);
-//   imageUrl = `${baseUrl}${imageUrl}`;
-//   // Fetch image blob
-//   const blob = await fetchImageBlob(imageUrl);
-//   // Convert to Base64 string so it can be passed to Netlify function
-//   const base64 = await blobToBase64(blob);
-//   return await callNetlifyUploadFunction('fetchAndUploadImage', {
-//     file: base64,
-//   });
-// }
-
-// Get image from Supabase Storage
-// export async function getImageFromSupabase(path) {
-//   const url = await callNetlifyUploadFunction('getImageFromSupabase', {
-//     path,
-//   });
-//   console.log('REACT URL', url);
-//   return url;
-// }
